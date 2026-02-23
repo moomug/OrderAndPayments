1ff6dc8434886da3bc011ada1de97d2bd6209499
moomug <moghrabi.eng@outlook.com>
2026-02-23 20:08:42 +0100

    first commit

---
+++ Enums/PaymentMethod.cs
@@ -0,0 +1,10 @@
+
+namespace M03.OrderPaymentSystem.OrderServiceApi.Requests;
+
+public enum PaymentMethod
+{
+    CreditCard,
+    DebitCard,
+    PayPal,
+    BankTransfer
+}
---
+++ Services/IOrderService.cs
@@ -0,0 +1,12 @@
+using M03.OrderPaymentSystem.OrderServiceApi.Requests;
+using M03.OrderPaymentSystem.OrderServiceApi.Responses;
+
+namespace M03.OrderPaymentSystem.OrderServiceApi.Services;
+
+public interface IOrderService
+{
+    Task<OrderResponse?> GetByIdAsync(Guid orderId, CancellationToken cancellationToken = default);
+    Task<OrderResponse> CreateAsync(CreateOrderRequest request, CancellationToken cancellationToken = default);
+    Task PayAsync(Guid orderId, PaymentRequest request, CancellationToken cancellationToken = default);
+    Task CancelAsync(Guid orderId, CancellationToken cancellationToken = default);
+}
